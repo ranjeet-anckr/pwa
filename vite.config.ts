959f898e5460d10dcba859e3e7755ece28e65f13
ranjeet-anckr <ranjeet.kumar@xotiv.com>
2024-12-16 16:38:04 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,61 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "autoUpdate", 
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.png'],
+  manifest: {
+    name: "Weather App",
+    short_name: "WeatherApp",
+    description: "An app that shows the weather forecast for your city.",
+    icons: [
+      {
+        src: "./icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "./icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any"
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "any"
+      },
+      {
+        src: "./icon-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+        purpose: "any"
+      },
+      {
+        src: "./icon-256x256.png",
+        sizes: "256x256",
+        type: "image/png",
+        purpose: "any"
+      },
+      {
+        src: "./icon-384x384.png",
+        sizes: "384x384",
+        type: "image/png",
+        purpose: "maskable"
+      }
+    ],
+    theme_color: "#181818",
+    background_color: "#e8eac2",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait-primary"
+  }
+};
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA(manifestForPlugin)],
-  
-})
+});
